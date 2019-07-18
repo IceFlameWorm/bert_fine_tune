199148b4ad1b88097aa70c8aad36074838185b43
aaron <aaron@empty.com>
2019-07-18 07:53:48 +0000

    改正LCQMCProcessor中的一个小错误

--- examples/run_classifier_dataset_utils_fine_tune.py
+++ examples/run_classifier_dataset_utils_fine_tune.py
@@ -456,7 +456,7 @@ class LCQMCProcessor(DataProcessor):
         return self._create_examples(fp, 'dev')
 
     def get_test_examples(self, data_dir):
-        fp = os.path.join(data, 'test.csv')
+        fp = os.path.join(data_dir, 'test.csv')
         return self._create_examples(fp, 'test')
 
     def get_labels(self):
