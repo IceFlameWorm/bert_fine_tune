12e45a544b11eea6a056b19fc4a38c96f75edcd4
aaron <aaron@empty.com>
2019-07-24 06:46:17 +0000

    保存训练脚本

--- examples/run_classifier_fine_tune.sh
+++ examples/run_classifier_fine_tune.sh
@@ -1,16 +1,17 @@
-ATEC_CCKS_PATH=/efs/projects/bert_fine_tune/fine_tune/data/train_dev_test/ATEC_CCKS/processed
-#BERT_MODEL_PATH=/efs/downloads/bert/pytorch/bert_base_chinese
-BERT_MODEL_PATH=/efs/fine_tune/atec_ccks_fine_tune_3/
+DATA_DIR=/efs/projects/bert_fine_tune/fine_tune/data/train_dev_test/LCQMC/processed
+BERT_MODEL_PATH=/efs/downloads/bert/pytorch/bert_base_chinese
+#BERT_MODEL_PATH=/efs/fine_tune/lcqmc/pointwise/lcqmc_fine_tune_40_1_5e-6/
+#BERT_MODEL_PATH=/efs/fine_tune/lcqmc/pointwise/lcqmc_fine_tune_sample/
 
-python run_classifier_fine_tune.py \
-  --task_name atec_ccks \
+python run_classifier_pointwise.py \
+  --task_name lcqmc \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --data_dir  $ATEC_CCKS_PATH \
+  --data_dir  $DATA_DIR \
   --bert_model $BERT_MODEL_PATH \
-  --max_seq_length 128 \
+  --max_seq_length 40 \
   --train_batch_size 32 \
-  --learning_rate 2e-5 \
+  --learning_rate 8e-6 \
   --num_train_epochs 1.0 \
-  --output_dir /efs/fine_tune/atec_ccks_fine_tune_4/
+  --output_dir /efs/fine_tune/lcqmc/pointwise/lcqmc_fine_tune_40_1_8e-6/
---
+++ examples/run_classifier_fine_tune_1.sh
@@ -0,0 +1,26 @@
+DATA_SET=LCQMC
+TASK_NAME=$(echo ${DATA_SET} | tr '[A-Z]' '[a-z]')
+MODEL_TYPE=pointwise
+LEARNING_RATE=8e-6
+EPOCHS=1.0
+EPOCH_SPAN=2
+MAX_SEQ_LENGTH=40
+
+DATA_DIR=/efs/projects/bert_fine_tune/fine_tune/data/train_dev_test/${DATA_SET}/processed
+#BERT_MODEL_PATH=/efs/downloads/bert/pytorch/bert_base_chinese
+BERT_MODEL_PATH=/efs/fine_tune/${TASK_NAME}/pointwise/${TASK_NAME}_fine_tune_40_1_8e-6/
+#BERT_MODEL_PATH=/efs/fine_tune/lcqmc/pointwise/lcqmc_fine_tune_sample/
+
+
+python run_classifier_${MODEL_TYPE}.py \
+  --task_name ${TASK_NAME} \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir  ${DATA_DIR} \
+  --bert_model ${BERT_MODEL_PATH} \
+  --max_seq_length ${MAX_SEQ_LENGTH} \
+  --train_batch_size 32 \
+  --learning_rate ${LEARNING_RATE} \
+  --num_train_epochs ${EPOCHS} \
+  --output_dir /efs/fine_tune/${TASK_NAME}/${MODEL_TYPE}/${TASK_NAME}_fine_tune_${MAX_SEQ_LENGTH}_${EPOCH_SPAN}_${LEARNING_RATE}/
